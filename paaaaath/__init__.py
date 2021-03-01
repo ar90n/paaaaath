aed2c81f59e398bb1c173c8523da1b26eba56963
Masahiro Wada <argon.argon.argon@gmail.com>
2021-03-02 03:56:48 +0900

    chore: add verion

--- paaaaath/__init__.py
+++ paaaaath/__init__.py
@@ -5,3 +5,10 @@ from .s3 import PureS3Path, S3Path
 from .gcs import PureGCSPath, GCSPath
 from .uri import PureUriPath
 from .windows import PureWindowsPath, WindowsPath
+
+from importlib.metadata import version
+
+try:
+    __version__ = version(__name__)
+except:
+    pass
